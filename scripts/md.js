758061320e11f7da0d9558f8a8b579303717c028
liulifu <initlifu@hotmail.com>
2024-09-27 18:31:43 +0800

    add

--- scripts/md.js
+++ scripts/md.js
@@ -1,4 +1,14 @@
-// Function to change the style
+window.addEventListener("message", function (event) {
+  if (event.data.action === "change-style") {
+    changeStyle(event.data.styleName);
+  }
+});
+
+// Initialize the selected style from localStorage or default to 'modern'
+const savedStyle = localStorage.getItem("preferredStyle") || "Old_book";
+document.getElementById("style-select").value = savedStyle;
+changeStyle(savedStyle);
+
 function changeStyle(styleName) {
   const styles = {
     modern: document.getElementById("modern-style"),
@@ -6,50 +16,18 @@ function changeStyle(styleName) {
     minimalist: document.getElementById("minimalist-style"),
     retro: document.getElementById("retro-style"),
     neon: document.getElementById("neon-style"),
-    old_book: document.getElementById("old_book_style_resume"), // Add the old_book style
+    old_book: document.getElementById("old_book_style_resume"),
   };
 
-  // Disable all styles
+  // 关闭所有样式
   Object.keys(styles).forEach((key) => {
     if (styles[key]) {
       styles[key].disabled = true;
     }
   });
 
-  // Enable the selected style
+  // 启用所选样式
   if (styles[styleName]) {
     styles[styleName].disabled = false;
   }
-
-  // Save the selected style in localStorage
-  localStorage.setItem("preferredStyle", styleName);
 }
-
-// Initialize the selected style from localStorage or default to 'modern'
-const savedStyle = localStorage.getItem("preferredStyle") || "modern";
-document.getElementById("style-select").value = savedStyle;
-changeStyle(savedStyle);
-
-// Listen for changes in the style select dropdown
-document.getElementById("style-select").addEventListener("change", function () {
-  changeStyle(this.value);
-});
-
-// // 回到顶部的功能
-// document.getElementById('back-to-top').addEventListener('click', function(event) {
-//     event.preventDefault(); // 防止默认跳转行为
-//     window.scrollTo({
-//       top: 0,
-//       behavior: 'smooth'  // 平滑滚动到顶部
-//     });
-//   });
-
-//   document.addEventListener('DOMContentLoaded', function() {
-//     document.getElementById('back-to-top').addEventListener('click', function(event) {
-//       event.preventDefault(); // 防止默认跳转行为
-//       window.scrollTo({
-//         top: 0,
-//         behavior: 'smooth'  // 平滑滚动到顶部
-//       });
-//     });
-//   });
